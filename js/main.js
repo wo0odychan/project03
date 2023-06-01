a37804bd3d63ce2315e598bcd1e04b90a0dcc1c7
woojin <133613646+wo0odychan@users.noreply.github.com>
2023-06-01 17:26:11 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -61,7 +61,16 @@ $(function () {
                 slidesToShow: 1,
             }
         }]
-        // 
     });
+    $('#product .arrows .left').on('click', function () {
+        $('.product_slide').slick('slickPrev')
+    });
+    $('#product .arrows .right').on('click', function () {
+        $('.product_slide').slick('slickNext');
+    });
+    // 누르면 위까지 올라가는 거
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 1000)
+    })
 
 })
